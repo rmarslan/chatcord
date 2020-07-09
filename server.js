17a1e7cfff664d36dda9fbe41affb4e10fd65220
arslan <rmarslanrana@gmail.com>
2020-07-09 15:56:27 +0500

    setup the public folder

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.NODE_ENV || 3000;
 app.listen(PORT, () => {
